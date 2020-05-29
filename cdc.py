3b2771c2f6b0f3ff190b33b009c6c7aaecce15ac
Warren Focke <wbfocke@gmail.com>
2020-05-29 13:12:21 -0700

    use moving avg

--- cdc.py
+++ cdc.py
@@ -10,49 +10,32 @@ plt.ion()
 
 import numpy as np
 
+skipBytes = 3
+skipItems = 1
 days = 7
-
-def plot(*args):
-    fig, ax = plt.subplots()
-    for yy in args:
-        xx = np.arange(len(yy))
-        ee = np.sqrt(yy)
-        ax.plot(xx, yy)
-        ax.fill_between(xx, yy-ee, yy+ee, alpha=0.2)
-        continue
-    return
-
 fn = "new-cases-chart-data.json"
 
-raw = eval(open(fn).read())
-start = (len(raw[0]) - 1) % days + 1
+raw = json.loads(open(fn).read()[skipBytes:])
 
-allTimes = np.array([time.mktime(time.strptime(xx, '%m/%d/%Y')) for xx in raw[0][1:]])
-times = np.array([time.mktime(time.strptime(xx, '%m/%d/%Y')) for xx in raw[0][start:]])
+times = np.array([time.mktime(time.strptime(xx, '%m/%d/%Y')) for xx in raw[0][skipItems:]])
 times -= times[0]
 times /= 86400
-times.shape = (-1, days)
-times = np.round(times.transpose())
+times = np.round(times)
+times += 0.5
 
-allCooked = np.array([float(xx) for xx in raw[1][1:]])
-ma = np.convolve(allCooked, np.ones(days)/days, mode='valid')
+cases = np.array([float(xx) for xx in raw[1][skipItems:]])
 
-cooked = np.array([float(xx) for xx in raw[1][start:]])
-cooked.shape = (-1, days)
-cooked = cooked.transpose()
+kernel = np.ones(days)/days
 
-byWeek = np.add.reduce(cooked, 0)
-byDay = np.add.reduce(cooked, 1)
-nDay = np.add.reduce(cooked / byWeek, 1)
-
-plot(byDay)
-plot(byWeek)
-plot(nDay)
-plot(ma)
+maTimes = np.convolve(times, kernel, mode='valid')
+maCases = np.convolve(cases, kernel, mode='valid')
 
 fig, ax = plt.subplots()
-for ii in range(len(times)):
-    ax.plot(times[ii], cooked[ii], alpha=0.2)
+for ii in range(days):
+    ax.plot(times[ii::7], cases[ii::7], alpha=0.3)
     continue
+
+ax.plot(maTimes, maCases)
+ax.plot(times, cases, alpha=0.2)
 
 plt.show()
